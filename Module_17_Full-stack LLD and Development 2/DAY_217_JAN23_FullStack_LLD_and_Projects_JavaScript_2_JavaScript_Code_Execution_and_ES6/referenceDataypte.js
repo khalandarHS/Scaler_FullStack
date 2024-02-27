4a92f47ebc8278cec27fd9b2e629e4cb19d5c6ab
khalandarHS <khalandar.h.s@gmail.com>
2024-02-27 21:16:51 +0530

    class 2 added

---
+++ Module_17_Full-stack LLD and Development 2/DAY_217_JAN23_FullStack_LLD_and_Projects_JavaScript_2_JavaScript_Code_Execution_and_ES6/referenceDataypte.js
@@ -0,0 +1,12 @@
+const obj = {
+    firstName: "Khal",
+    lastName: "shaik",
+};
+
+// spread operator
+const obj2 = obj;
+obj2.firstName = "uttam";
+obj2.lastName ="sharma";
+
+console.log(obj);
+console.log(obj2);
